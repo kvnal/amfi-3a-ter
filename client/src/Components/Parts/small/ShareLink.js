9a64d8e50e5698eb89217ac7ce24a9751d28e481
kvnal <72142540+kvnal@users.noreply.github.com>
2021-07-15 21:20:51 +0530

    ShareLink component added

---
+++ client/src/Components/Parts/small/ShareLink.js
@@ -0,0 +1,34 @@
+import Share from "@material-ui/icons/ShareRounded";
+import toast , {Toaster} from 'react-hot-toast';
+
+const ShareLink = () => {
+    const handleShare = ()=>{
+        const url = window.location.href
+        var inp = document.body.appendChild(document.createElement("input"))
+        inp.value = url
+        inp.focus()
+        inp.select()
+        inp.setSelectionRange(0,1000)
+        document.execCommand('copy')
+        inp.parentNode.removeChild(inp)
+        window.scrollTo(0,0)
+        return toast.success("Copied")
+
+    }
+    return (<>
+              <span onClick={handleShare}>
+                    <Share />
+                    <Toaster 
+            position="bottom-center"
+            reverseOrder={false}
+            toastOptions={{
+                success:{
+                    duration: 1500
+                }}
+            }
+            />
+                  </span>
+    </>);
+};
+
+export default ShareLink;
